64c1b03e4e142f939e043aa480d5c9095830d3c4
Piotr Arczi <piotr.borysowski@gmail.com>
2019-01-19 21:27:59 +0100

    First commit

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
+
 const PORT = process.env.PORT || 5000
 
+const publicApiKey = process.env.PUBLIC_API_KEY
+const paylane_login = process.env.PL_LOGIN
+const paylane_pass = process.env.PL_PASS
+
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({
+    extended: true
+  }))
+  .use(cors({ origin: true }))
+  .get('/', (req, res) => res.send('API'))
+  .post('/api/generateToken', function (req, res) {
+    const ccdata = req.body;
+    ccdata.public_api_key = publicApiKey;
+
+    axios.post('https://direct.paylane.com/rest.js/cards/generateToken', ccdata)
+      .then(response => {
+        res.send(response.data)
+      })
+      .catch(error => {
+        res.send(error)
+      })
+
+  })
+  .post('/api/saleByToken', function (req, res) {
+    const ccdata = req.body;
+  axios.post(encodeURI(`https://${paylane_login}:${paylane_pass}@direct.paylane.com/rest/cards/saleByToken`), ccdata)
+    .then(response => {
+      res.send(response.data)
+    })
+    .catch(error => {
+      res.send(error)
+    })
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))

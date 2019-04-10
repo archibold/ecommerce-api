6ecf9eb8af7c2a88941227cd377c194fd4274257
Piotr Arczi <piotr.borysowski@gmail.com>
2019-04-10 19:06:16 +0200

    check img

--- api/product.js
+++ api/product.js
@@ -51,20 +51,30 @@ exports.putProduct = function(req, res) {
   const uid = firebase.auth().currentUser.uid;
   const data = req.body;
 
-  // if (!data.description) res.status(404).send('no description');
+  if (!data.description) res.status(404).send('no description');
   if (!data.title) res.status(404).send('no title');
   if (!data.price) res.status(404).send('no price');
-  // if (!data.image) res.status(404).send('no image');
+  if (!data.image) res.status(404).send('no image');
+  
+    firebase.database().ref('images').push()
+    .set(image)
+    .then(function(img) {
+      console.log(img);
+      res.send(img);
 
-  firebase.database().ref('products/' + uid).push().set({
-    id: Math.random().toString(36).substr(2, 9),
-    description: data.description,
-    title: data.title,
-    price: data.price,
-    image: data.image
-  }).then(function() {
-    res.send();
-  }).catch(function(error) {
-    res.status(404).send('bad request');
-  })
+    }).catch(function(error) {
+      res.status(404).send('bad request');
+    });
+
+  // firebase.database().ref('products/' + uid).push().set({
+  //   id: Math.random().toString(36).substr(2, 9),
+  //   description: data.description,
+  //   title: data.title,
+  //   price: data.price,
+  //   image: data.image
+  // }).then(function() {
+  //   res.send();
+  // }).catch(function(error) {
+  //   res.status(404).send('bad request');
+  // })
 }
